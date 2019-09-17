0cf5c1cf25e66d63fd2307fa4c96ce9cfd481a7c
NWZX <NWZX@users.noreply.github.com>
2019-09-17 13:37:54 +0200

    Clean

--- TaquinGame/TaquinBase.c
+++ TaquinGame/TaquinBase.c
@@ -106,47 +106,6 @@ int** newPlat(int sizeX, int sizeY, int random)
 	return plat;
 }
 
-void newScreen(int cursX, int cursY, int* cursX2, int* cursY2, int** plat, int size)
-{
-	system("cls");
-	for (int i = 0; i < size; i++)
-	{
-		for (int l = 0; l < size; l++)
-		{
-			if (cursX2 != NULL && cursY2 != NULL && i == *cursX2 && l == *cursY2)
-			{
-				printf("|+%02d", plat[i][l]);
-			}
-			else if (i == cursX && l == cursY)
-			{
-				printf("|*%02d", plat[i][l]);
-			}
-			else
-				printf("| %02d", plat[i][l]);
-		}
-		printf("|\n");
-	}
-}
-
-int interceptKey(int* cursX, int* cursY)
-{
-	int key = _getch();
-	if (key == 72 && *cursX > 0) // UP
-		* cursX -= 1;
-	else if (key == 80 && *cursX < 3) // DOWN
-		* cursX += 1;
-	else if (key == 75 && *cursY > 0) // LEFT
-		* cursY -= 1;
-	else if (key == 77 && *cursY < 3) // RIGHT
-		* cursY += 1;
-	else if (key == 13) // Enter
-		return 1;
-	else if (key == 113) // Q
-		return -1;
-
-	return 0;
-}
-
 void validateMove(int cursX, int cursY, int* cursX2, int* cursY2, int** plat)
 {
 	if (cursX2 == NULL || cursY2 == NULL)
--- TaquinGame/TaquinBase.h
+++ TaquinGame/TaquinBase.h
@@ -30,6 +30,4 @@ int** tab2D_init(int x, int y);
 int* tab2D_convert(int** tabXY, int dimSize);
 void tab2D_free(int** tabXY, int x);
 int** newPlat(int sizeX, int sizeY, int random);
-void newScreen(int cursX, int cursY, int* cursX2, int* cursY2, int** plat, int size);
-int interceptKey(int* cursX, int* cursY);
 void validateMove(int cursX, int cursY, int* cursX2, int* cursY2, int** plat);
