3b326511ed724e6da1648e15e8dbec74f3a17692
NWZX <NWZX@users.noreply.github.com>
2019-10-29 07:32:23 +0100

    Add controller for Server

---
+++ TaquinServer/Controllers/GameAPIController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaquinServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameAPIController : ControllerBase
+    {
+        // GET: api/GameAPI
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GameAPI/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GameAPI
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/GameAPI/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
